935045588709061e0a2a9edae54a66a9c1303369
Terence Le Thierry <terence.lethierry@protonmail.com>
2023-12-17 17:34:47 +0100

    Ajout classe Application dans main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Script principal
+from tkinter import *
+
+class Application(Tk):
+    "Application"
+    def __init__(self):
+        super().__init__() # Hériter des attributs de la classe Tk
+
+        self.barre_menus = Menu(self, tearoff=0) # Ajouter une barre de menus
+        self.menu_fichier = Menu(self.barre_menus, tearoff=0) # Ajouter un menu "Fichier"
+        self.barre_menus.add_cascade(label="Fichier", menu=self.menu_fichier)
+
+        self.config(menu=self.barre_menus) # On configure le menu de la fenêtre
+
+
+
+app = Application()
+app.mainloop()
